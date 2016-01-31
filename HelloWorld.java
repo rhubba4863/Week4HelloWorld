8db0172ae73ac1bc3f46026a134cae397a9c8317
Parker Hubbard <hub13016@byui.edu>
2016-01-31 17:41:23 -0500

    ADDED EXTRA TEXT, (testing... 1,2,3)

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello GitHub World");
+        System.out.println("Testing...");
     }
     
 }
